6ff96fae89b23e4effcc627634567f7ddf8e5583
Whitney <whitneyyx77@gmail.com>
2025-02-12 22:48:56 -0500

    Completed task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -71,8 +71,45 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    try {
+        // Get author from request parameters
+        const requestedAuthor = req.params.author;
+
+        // Check if author parameter is provided
+        if (!requestedAuthor) {
+            return res.status(400).json({
+                message: "Author parameter is required"
+            });
+        }
+
+        // Get all book ISBNs
+        const bookISBNs = Object.keys(books);
+
+        // Filter books by the requested author
+        const authorBooks = bookISBNs.reduce((result, isbn) => {
+            if (books[isbn].author.toLowerCase() === requestedAuthor.toLowerCase()) {
+                result[isbn] = books[isbn];
+            }
+            return result;
+        }, {});
+
+        // Check if any books were found
+        if (Object.keys(authorBooks).length > 0) {
+            return res.status(200).json({
+                message: "Books found for author: " + requestedAuthor,
+                books: authorBooks
+            });
+        } else {
+            return res.status(404).json({
+                message: "No books found for author: " + requestedAuthor
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error retrieving books",
+            error: error.message
+        });
+    }
 });
 
 // Get all books based on title
